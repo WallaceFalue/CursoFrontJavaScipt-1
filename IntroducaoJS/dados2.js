37bac62cb6703a15ba24ccc1a5f1b0202f79d585
WallaceFalue <wallaceffalque@gmail.com>
2025-04-01 21:29:32 -0300

    CursoDay1#1

---
+++ IntroducaoJS/dados2.js
@@ -0,0 +1,16 @@
+let preco = 19.90;;
+// 40% = x*0.4
+let desconto = 0.4;;
+
+console.log(19.9 * 0.6);
+console.log(preco * (1 - desconto));
+
+// Sequencia de simbulos == string 
+let nome = "caderno";
+let categoria = "Papelaria";
+console.log("Produlto: " + nome
+    + ", Categora: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
+    
+console.log(1 + 1);
---
+++ IntroducaoJS/dados4.js
@@ -0,0 +1,4 @@
+const a = 3;
+// alterar valor da variável
+//a = a + 10
+console.log(a);
